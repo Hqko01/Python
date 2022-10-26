8c5750f93bf9dad487f3d767e207202eb0f184ba
Hqko <74988116+Hqko01@users.noreply.github.com>
2022-10-27 00:01:59 +0300

    Delete Piramit3.py

--- Piramit3.py
+++
@@ -1,9 +0,0 @@
-def PiramiThree(x):
-    star = "*"
-    space = " "
-    for k in range(1, x, 1):
-        print(".")
-
-    for i in range(1, x, 1):
-        print((space * (i - k)) + (star * i) + (space * i) + (star * i))
-PiramiThree(20)
