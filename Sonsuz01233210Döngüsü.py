ba1761a2914349ede7aa93f91a3a31b4941b45ce
Hqkko <74988116+Hqko01@users.noreply.github.com>
2023-05-08 22:29:04 +0300

    Delete Sonsuz01233210Döngüsü.py

--- Sonsuz01233210Döngüsü.py
+++
@@ -1,9 +0,0 @@
-while True:
-    def d(x):
-        b = " "
-        for i in range(0, x, 1):
-            print(i, b, end="")
-        for a in range(3, -1, -1):
-            print(a, b, end="")
-        print(" ")
-    d(4)
